141de4c2ec2079f7c0a819d4366739358ffe7f52
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-04 15:45:50 +0300

    Cклад товаров 16
    Функция checkStorage(available, ordered) проверяет возможность оформления заказа и возвращает сообщение о результате. Она объявляет два параметра, значения которых будут задаваться во время её вызова:
    
    available - общее количество товаров на складе
    ordered - единиц товара в заказе
    Используя ветвления дополни код функции так, что:
    
    Если в заказе указано число, превышающее количество товаров на складе, в переменную message записывается строка 'На складе недостаточно товаров!'.
    В противном случае записывается строка 'Заказ оформлен, с вами свяжется менеджер'.

---
+++ js/script16.js
@@ -0,0 +1,9 @@
+function checkStorage(available, ordered) {
+  let message;
+  // Пиши код ниже этой строки
+  if(available < ordered){
+ message = 'На складе недостаточно товаров!';
+ } else{ message = 'Заказ оформлен, с вами свяжется менеджер'};
+  // Пиши код выше этой строки
+  return message;
+}
