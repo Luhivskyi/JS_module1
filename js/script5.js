4bf3462437d563e009de4d663ec6d695146bbdbe
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-04 13:50:00 +0300

    Шаблонные строки 5
    Объяви перемнную message и запиши в неё сообщение о покупке, строку в формате: Вы выбрали <имя товара>, цена за штуку <цена товара> кредитов. Где <имя товара> и <цена товара> это значения переменных productName и pricePerItem. Используй синтаксис шаблонных строк.

---
+++ js/script5.js
@@ -0,0 +1,5 @@
+// Базовый код
+const productName = 'Дроид';
+const pricePerItem = 3500;
+const message = `Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов`;
+// Пиши код ниже этой строки
