7e0b97b12aa93b1d5502f2f1fd5081e143da4ac8
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-05 19:09:07 +0300

    Проверка спама  36
    Функция checkForSpam(message) принимает строку (параметр message), проверяет её на содержание запрещённых слов spam и sale, и возвращает результат проверки. Слова в строке параметра message могут быть в произвольном регистре, например SPAM или sAlE.
    
    Если нашли запрещённое слово (spam или sale) то функция возвращает буль true.
    Если в строке нет запрещённых слов, функция возвращает буль false.

---
+++ js/script36.js
@@ -0,0 +1,7 @@
+function checkForSpam(message) {
+  let result;
+  
+ const checkForSpam = message;
+  
+  return message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+}
